04a987d278afe824b3c8891b6324b9ef5a4797d7
whoiscaio <whoiscaio@protonmail.com>
2023-07-18 14:19:30 -0300

    feat: jwt service implemented

---
+++ src/config/jwt.config.ts
@@ -0,0 +1,5 @@
+export const jwtConstants = {
+  secret:
+    process.env.JWT_SECRET ||
+    '644cf8c601a9cb03f2dde74c3284f9ee8b9836772620d6d49830390ffc8b711c',
+};
---
+++ src/shared/services/jwt.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtSignOptions, JwtService as NestJwtService } from '@nestjs/jwt';
+import { jwtConstants } from 'src/config/jwt.config';
+
+@Injectable()
+export class JwtService {
+  private readonly jwtService: NestJwtService;
+
+  constructor() {
+    this.jwtService = new NestJwtService({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '2h' },
+    });
+  }
+
+  sign(payload: any, options?: JwtSignOptions) {
+    return this.jwtService.sign(payload, options);
+  }
+
+  verify(jwt: string) {
+    try {
+      return this.jwtService.verify(jwt);
+    } catch (error) {
+      throw new UnauthorizedException('Token inválido ou expirado');
+    }
+  }
+}
