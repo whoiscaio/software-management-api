a96c0950eaec0bb5fe2934c132a2bcb9932b354f
whoiscaio <whoiscaio@protonmail.com>
2023-07-18 16:59:17 -0300

    feat: create and edit team methods

--- src/modules/team/controllers/team.controller.ts
+++ src/modules/team/controllers/team.controller.ts
@@ -1,14 +1,50 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { TeamService } from '../services/team.service';
+import { TeamDTO } from '../dtos/team.dto';
 
 @Controller('teams')
+@UsePipes(ValidationPipe)
 export class TeamController {
   constructor(private readonly teamService: TeamService) {}
 
   @Get()
   async findAll() {
-    const teams = this.teamService.getTeams();
+    const teams = await this.teamService.getAll();
 
     return teams;
+  }
+
+  @Post()
+  async create(@Body() teamDTO: TeamDTO) {
+    await this.teamService.create(teamDTO);
+
+    return;
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() teamDTO: TeamDTO,
+  ) {
+    const existingTeam = await this.teamService.getOne(id);
+
+    if (!existingTeam) {
+      throw new NotFoundException('Team not found');
+    }
+
+    const updatedTeam = await this.teamService.update(id, teamDTO);
+
+    return updatedTeam;
   }
 }
--- src/modules/team/services/team.service.ts
+++ src/modules/team/services/team.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Team } from '../models/team.model';
+import { TeamDTO } from '../dtos/team.dto';
 
 @Injectable()
 export class TeamService {
@@ -10,9 +11,34 @@ export class TeamService {
     private readonly teamRepository: Repository<Team>,
   ) {}
 
-  async getTeams() {
+  async getAll() {
     const teams = await this.teamRepository.find();
 
     return teams;
+  }
+
+  async getOne(id: string) {
+    const team = await this.teamRepository.findOne({ where: { id } });
+
+    return team || null;
+  }
+
+  async create(team: TeamDTO) {
+    const newTeam = this.teamRepository.create(team);
+
+    await this.teamRepository.save(newTeam);
+  }
+
+  async update(id: string, team: TeamDTO) {
+    const existingTeam = await this.getOne(id);
+
+    const updatedTeamModel = this.teamRepository.create({
+      ...existingTeam,
+      ...team,
+    });
+
+    const updatedTeam = await this.teamRepository.save(updatedTeamModel);
+
+    return updatedTeam;
   }
 }
