5c788398807f635b3803526321867910d18fbb0a
whoiscaio <whoiscaio@protonmail.com>
2023-07-24 00:33:02 -0300

    docker: compose

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 const TypeOrmModuleConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'root',
-  password: 'root',
-  database: 'db_sw_process_management',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USERNAME || 'root',
+  password: process.env.DB_PASSWORD || 'root',
+  database: process.env.DB_DATABASE || 'db_sw_process_management',
   entities: ['dist/modules/**/models/*.model.js'],
   synchronize: true,
 };
